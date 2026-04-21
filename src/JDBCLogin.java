8bdaacfa6f19e32d8ed4aab7a121a303d5ae1ab2
Prasanth <prasanth530001@gmail.com>
2026-04-21 21:18:11 +0530

    Prepared Statement

--- src/JDBCLogin.java
+++ src/JDBCLogin.java
@@ -35,7 +35,20 @@ public class JDBCLogin {
         // if(rs.next()) System.out.println("User Logged in...");
         // else System.out.println("Invalid Credentials...");
         //
+        // NOTE ----
+        // we can also get the names of columns by doing - ResultSetMetaData metaData = rs.getMetaData();
+        // now metaData.getColumnCount() - gives the count/ no of columns
+        // and metaData.getColumnName() - gives the name of the column
+        // ----
+        //
 
         System.out.println("User Signed up...");
+
+        // -----
+        // NOTE (Check D3 MetaData Class) - handle the constants like url, username, password
+        // and connection and statement with methods in separate packages like util, constant...
+        // helps in organizing our project in clear way (good architecture)
+        // Try this too..
+        // -----
     }
 }
---
+++ src/PreparedStatement.java
@@ -0,0 +1,33 @@
+// initially we used Statement, but with Statement there is possibility of getting effected with
+// 'SQL Injection'. to overcome that, 'Prepared Statement' is introduced.
+// Prepared Statement is like an extension for Statement. it comes with a pre-compiled query.
+
+// In Statement, we first create statement, and then prepare query and we execute.
+// but in Prepared Statement, we first prepare query, and then create statement and execute.
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PreparedStatement {
+    public static void main(String[] args) throws SQLException {
+
+        // if we handle boilerplate code in separate util package, the code will looks like,
+        Connection connection = DatabaseConnection.getConnection();
+
+        // Ex: "Insert into users values(?, ?, ?)";
+        String query = "SELECT * from users where id = ? and name = ?"; // we can inject values dynamically instead of hardcoding with '?'
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setInt(1, 2); // 1 -> will be which ? ,and 2 -> is the value
+        statement.setString(2, "Prasanth");
+
+        ResultSet rs = statement.executeQuery();
+
+        while(rs.next()) {
+            System.out.println(rs.getInt(1) + " " + rs.getString(1));
+        }
+
+        // there are also addBatch() and executeBatch() methods which helps in adding in batches and
+        // execute as batches.
+    }
+}
